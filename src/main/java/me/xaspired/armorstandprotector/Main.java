f8b029a1473123722c4e9d6be50cd5085fcc90ce
xAspired <pinoeninfa@libero.it>
2025-05-10 08:38:58 +0200

    feat: basic ArmorStandProtector plugin setup

---
+++ src/main/java/me/xaspired/armorstandprotector/Main.java
@@ -0,0 +1,10 @@
+package me.xaspired.armorstandprotector;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+}
